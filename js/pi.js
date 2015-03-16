f3996afdba7ba9126e0beed258865929d1bd73d7
jillkahn <jkahn@mail.smcvt.edu>
2015-03-15 22:04:36 -0400

    Added multiplication by 4 to correct calculation

--- js/pi.js
+++ js/pi.js
@@ -1,13 +1,25 @@
 console.log('hello world');
-var r=4;
-var circleArea=0;
-$(document).ready(function() {
+
+var calcPi = function(r) {
+	var circleArea=0;
 	for (var i = 0;i<r+1; i++) {
 		for(var count=0;count<=r;count++){
 			if(Math.pow(i,2)+Math.pow(count,2)<=Math.pow(r,2))
 				circleArea++;
 		}
 	}
+	circleArea=circleArea*4;
 	var pi=circleArea/Math.pow(r,2);
 	$('#answer').text(pi);
+	console.log(circleArea);
+}
+
+$(document).ready(function() {
+	$( "form" ).submit(function( event ) {
+		event.preventDefault();
+
+		var r = $('#radius').val();
+		calcPi(r);
+	});
+
 });
