a2d1744aa31ec82ae4e285f2ab0be974b94fb604
jillkahn <jkahn@mail.smcvt.edu>
2015-03-15 21:40:32 -0400

    pi calculation

--- js/pi.js
+++ js/pi.js
@@ -1,5 +1,13 @@
 console.log('hello world');
-
+var r=4;
+var circleArea=0;
 $(document).ready(function() {
-	$('#answer').text(1+1);
+	for (var i = 0;i<r+1; i++) {
+		for(var count=0;count<=r;count++){
+			if(Math.pow(i,2)+Math.pow(count,2)<=Math.pow(r,2))
+				circleArea++;
+		}
+	}
+	var pi=circleArea/Math.pow(r,2);
+	$('#answer').text(pi);
 });
